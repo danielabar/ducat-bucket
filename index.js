f4c57ab465dd3e48e21c3a1c198e49487af70b83
Daniela Baron <baron.daniela@gmail.com>
2019-10-12 17:22:34 -0400

    fill buckets with zeroes when needed

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 function combos(n, k) {
-  console.log(`(${n}, ${k})`)
   let outer = []
   if (k === 1) {
-    console.log(`\tBASE n = ${n}`)
     return [
       [`${n}`]
     ]
   } else {
     for (let i = 0; i < n; i++) {
-      console.log(`\tFOR LOOP: ${i}`)
       let recurseResult = combos(n - i, k - 1)
       recurseResult.forEach(r => outer.push(`${i},${r}`))
     }
-    // FIXME: Should be as many zeroes as buckets minus 1
-    outer.push(`${n},0`)
+    outer.push(`${n},${Array(k - 1).fill('0').join(',')}`)
     return outer;
   }
 }
 
-console.log(combos(3, 3))
+const comb = combos(10, 5)
+console.log(`${JSON.stringify(comb, null, 2)}\nNum Combos = ${comb.length}`)
+// 4,598,126
